910f6573aa34bb72ecb23e332dbdfb4c9d06ffa0
Buzatel-Iulian <iulian.buzatel@gmail.com>
2022-10-24 22:56:29 +0300

    Added scripts and more configs

---
+++ backup.sh
@@ -0,0 +1,2 @@
+cp -r ~/.config/i3/** ./
+cp ~/.config/compton.conf ./compton.conf
---
+++ setup.sh
@@ -0,0 +1,4 @@
+apt install -y i3blocks i3 compton gnome-terminal nitrogen alsamixergui arandr autorandr caffeine caffeine-indicator blueman rofi vim
+
+cp -r ./** ~/.config/i3/
+cp compton.conf ~/.config/compton.conf
