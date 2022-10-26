33ef8a93d8b7a172055e465af1c6ed60c0061d12
Buzatel-Iulian <iulian.buzatel@gmail.com>
2022-10-27 00:15:43 +0300

    Screen saver, network speeds and more

--- bin/network_speed.sh
+++ bin/network_speed.sh
@@ -1,6 +1,31 @@
 #!/bin/sh
 
+interface_speed () {
+	first=$(grep $1 /proc/net/dev)
+	sec=$(sleep 1; grep $1 /proc/net/dev)
+	f=$(echo $first | cut -d " " -f $2)
+	s=$(echo $sec | cut -d " " -f $2)
+	echo "$(($s-$f))"
+		}
+
+scaling () {
+	if test $1 -gt 1000; then
+		echo "$(($1/1000)) K"
+	elif test $1 -gt 1000000; then
+		echo "$(($1/1000000)) M"
+	else
+		echo "$1 "
+	fi
+		}
+
 dwm_network_speed () {
+	down=$(interface_speed "wlp9s0" 2)
+	d=$(scaling $down)
+
+	up=$(interface_speed "wlp9s0" 10)
+	u=$(scaling $up)
+
+	echo $d"B/s ▼ "$u"B/s ▲"
     #get_bytes 'received_bytes' 'transmitted_bytes'
     #now=$(date +%s%N)
 
@@ -11,11 +36,11 @@ dwm_network_speed () {
     #    printf "Net %s %s"  "$(download_speed)" "$(upload_speed)"
     #fi
     #printf "%s\n" "$SEP2"
-first=$(grep wlp9s0 /proc/net/dev)
-sec=$(sleep 1; grep wlp9s0 /proc/net/dev)
-f=$(echo $first | cut -d " " -f 2)
-s=$(echo $sec | cut -d " " -f 2)
-echo "$(($s-$f)) B/s ▼"
+#first=$(grep wlp9s0 /proc/net/dev)
+#sec=$(sleep 1; grep wlp9s0 /proc/net/dev)
+#f=$(echo $first | cut -d " " -f 2)
+#s=$(echo $sec | cut -d " " -f 2)
+#echo "$(($s-$f)) B/s ▼"
 #awk '{if(l1){print $2-l1,$10-l2} else{l1=$2; l2=$10;}}' $first
 }
 
--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 apt install -y i3blocks i3 compton gnome-terminal nitrogen alsamixergui arandr autorandr caffeine caffeine-indicator blueman rofi vim
 
+apt-get install xscreensaver xscreensaver-data-extra xscreensaver-gl-extra -y
+
+
 cp -r ./** ~/.config/i3/
 cp compton.conf ~/.config/compton.conf
